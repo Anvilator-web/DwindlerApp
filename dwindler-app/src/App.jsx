09cec3d1196dd0eabcaa79b2d5608de290d1e456
Anvilator-web <ketanchowdhury@protonmail.com>
2022-07-30 16:11:01 +0530

    Setup navbar component

---
+++ dwindler-app/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/LeftNavBar/LeftNavBar.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <body className="App-body">
+        <div></div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
