1258a7154a2e3fc5274d5217e84396061121bca5
Anvilator-web <ketanchowdhury@protonmail.com>
2022-07-31 20:10:47 +0530

    Navbar ready

--- dwindler-app/src/App.jsx
+++ dwindler-app/src/App.jsx
@@ -1,22 +1,12 @@
-import "./App.css";
 import React from "react";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Home } from "./Home";
-import { About } from "./About";
-import { NoMatch } from "./NoMatch";
+import "./output.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <React.Fragment>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route component={NoMatch} />
-        </Switch>
-      </Router>
-    </React.Fragment>
+    <div className="app">
+      <NavBar />
+    </div>
   );
 }
 
