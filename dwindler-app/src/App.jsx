d5dce9497d49f1b2f99cd67cc2216dcabd0106d1
Anvilator-web <ketanchowdhury@protonmail.com>
2022-07-31 18:41:19 +0530

    im starting back from scratch

--- dwindler-app/src/App.jsx
+++ dwindler-app/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import React from "react";
-import { NavigationBar } from "./components/NavigationBar/NavigationBar";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Home } from "./Home";
 import { About } from "./About";
 import { NoMatch } from "./NoMatch";
-import SideBar from "./components/SideBar/SideBar";
 
 function App() {
   return (
@@ -17,8 +15,6 @@ function App() {
           <Route path="/about" component={About} />
           <Route component={NoMatch} />
         </Switch>
-        <NavigationBar />
-        <SideBar />
       </Router>
     </React.Fragment>
   );
